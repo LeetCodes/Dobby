ebb2171a0b6eb66e9f496dbe5efdebbc347069ec
ALEXANDRE CAZALA <alexandre.cazala@atos.net>
2017-08-17 10:35:50 +0200

    [FIX] AJOUT DES ENV VARS

--- configurations/mongodb.connector.js
+++ configurations/mongodb.connector.js
@@ -5,7 +5,7 @@
  * @date 07/07/2017
  */
 const mongoose = require('mongoose');
-const config = require('./config.secret').database;
+const config = require('./config').database;
 
 let connect = "mongodb://localhost/"+config.name;
 mongoose.connect(connect);
